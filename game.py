Solved two bugs: point_d being opposite of Direction.DOWN in SnakeGameAI._move and wrong index in target being Q-augmented (while calculating loss).

game.py
@@ -18,13 +18,13 @@ Point = namedtuple('Point', 'x, y')
 
 # rgb colors
 WHITE = (255, 255, 255)
-RED = (200,0,0)
+RED = (200, 0, 0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
-BLACK = (0,0,0)
+BLACK = (0, 0, 0)
 
 BLOCK_SIZE = 20
-SPEED = 20
+SPEED = 1000
 
 class SnakeGameAI:
     
@@ -80,7 +80,7 @@ class SnakeGameAI:
         # 4. place new food or just move
         if self.head == self.food:
             self.score += 1
-            reward = 1
+            reward = 10
             self._place_food()
         else:
             self.snake.pop()
@@ -126,7 +126,7 @@ class SnakeGameAI:
             new_dir = clock_wise[idx] #no change
         elif np.array_equal(action, [0, 1, 0]):
             next_idx = (idx + 1) % 4
-            new_dir = clock_wise[idx]
+            new_dir = clock_wise[next_idx]
         else:
             next_idx = (idx - 1) % 4
             new_dir = clock_wise[next_idx]

model.py
@@ -35,8 +35,8 @@ class QTrainer:
 
     def train_step(self, state, action, reward, next_state, done):
         state = torch.tensor(state, dtype=torch.float)
-        action = torch.tensor(state, dtype=torch.long)
-        next_state = torch.tensor(state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        next_state = torch.tensor(next_state, dtype=torch.float)
         reward = torch.tensor(reward, dtype=torch.float)
 
         if(len(state.shape) == 1):
@@ -55,7 +55,7 @@ class QTrainer:
             if not done[idx]:
                 Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
 
-            target[idx][torch.argmax(action).item()-2] = Q_new
+            target[idx][torch.argmax(action).item()] = Q_new
 
         # r + y * max(# next_predicted Q value) -> only do this if not done
         # pred.clone()